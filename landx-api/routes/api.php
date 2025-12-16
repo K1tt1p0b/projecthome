18e34b5c7319e97b123bd62f228c67bd718d460c
Aom-666 <Vachiravitcp1@gmail.com>
2025-12-16 09:06:38 +0700

    Del api

--- landx-api/routes/api.php
+++
@@ -1,8 +0,0 @@
-Route::prefix('v1')->group(function () {
-    Route::get('/health', function () {
-        return response()->json([
-            'success' => true,
-            'message' => 'API OK',
-        ]);
-    });
-});
