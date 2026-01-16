d17a063434dba5f88da9fc97bdd9b4f0d6c74bc0
K1tt1p0b <kittipob.jir@rmutto.ac.th>
2026-01-16 15:55:01 +0700

    แก้ page.js ในหน้า map v1

--- homez/src/app/(listing)/(map-style)/map-v1/page.js
+++ homez/src/app/(listing)/(map-style)/map-v1/page.js
@@ -1,21 +1,32 @@
-"use client";
-
-import { useEffect } from "react";
 import DefaultHeader from "@/components/common/DefaultHeader";
+
 import MobileMenu from "@/components/common/mobile-menu";
+
 import PropertyFilteringTwo from "@/components/listing/map-style/map-v1/PropertyFilteringTwo";
 
-export default function MapV1() {
-  useEffect(() => {
-    // ❌ ปิด AOS เฉพาะหน้านี้
-    document.documentElement.classList.remove("aos-init", "aos-animate");
-  }, []);
+import React from "react";
 
+export const metadata = {
+  title: "Map V1 || Homez - Real Estate NextJS Template",
+};
+
+const MapV1 = () => {
   return (
     <>
+      {/* Main Header Nav */}
       <DefaultHeader />
+      {/* End Main Header Nav */}
+
+      {/* Mobile Nav  */}
       <MobileMenu />
-      <PropertyFilteringTwo />
+      {/* End Mobile Nav  */}
+      <PropertyFilteringTwo/>
+
+      {/* <!-- Advance Feature Modal Start --> */}
+     
+      {/* Property Filtering */}
     </>
   );
-}
+};
+
+export default MapV1;
